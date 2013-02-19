d82f343b94c7ba4eee7dea2bf6413a893900d854
Tushar R. Gosavi <tugosavi@in.ibm.com>
2013-02-19 11:59:26 +0530

    Adding relative path for template directory

--- oquiz/settings.py
+++ oquiz/settings.py
@@ -107,7 +107,7 @@ ROOT_URLCONF = 'oquiz.urls'
 WSGI_APPLICATION = 'oquiz.wsgi.application'
 
 TEMPLATE_DIRS = (
-    "/home1/tushar/devel/python/oquiz/templates",
+    os.path.join(os.path.dirname(__file__), "..", "templates"),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
